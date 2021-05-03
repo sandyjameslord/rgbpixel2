1cff731cfc2673fbb0753df8583fbee4c163242a
Sandy Lord <sandylord@Sandys-Mac-mini.lan>
2021-05-03 13:50:57 -0400

    n-photo edit adjustments 6

--- pages/whatIsRGBPixel.vue
+++ pages/whatIsRGBPixel.vue
@@ -1,9 +1,10 @@
 <template>
     <main>
         <h1>Welcome to rgbPixel</h1><br>
-        <h2>rgbPixel allows you to save and animate your own 16x16 pictures, either hand created or converted from an image.</h2><br>
+        <h2>rgbPixel allows you to save and animate your own 16x16 rgbPixel Images. Hand created from the unique editor or converted from an image using client-side processing.</h2><br>
         <h2>rgbPixel converts any size image into a string of approximately 1kb, and uses only a MongoDB for storage - no AWS buckets necessary.</h2><br>
         <h2>Built with JavaScript, HTML5, CSS3, Vue, Nuxt, MongoDB, Express, and bCrypt.</h2><br>
+        <h6>Coming Soon: Use rgbPixel as your Hamburger Button with the rgbPixel node extension. More info soon (updated 5/3/2021)</h6>
         <h3>Please contact the creator, <a href="https://www.linkedin.com/in/sandy-lord-engineer/">Sandy Lord</a>, for any details or questions.</h3><br>
         <h3>Have fun!!</h3><br>
     </main>
