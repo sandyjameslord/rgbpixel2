5d04dd3c998b979b3992ce55d244770276802761
Sandy Lord <sandylord@Sandys-Mac-mini.lan>
2021-05-03 10:35:30 -0400

    making whatIsRGBPixel easier to read

--- pages/whatIsRGBPixel.vue
+++ pages/whatIsRGBPixel.vue
@@ -1,11 +1,11 @@
 <template>
     <main>
-        <h1>Welcome to rgbPixel</h1>
-        <h2>rgbPixel allows you to save and animate your own 16x16 pictures, either hand created or converted from an image.</h2>
-        <h2>rgbPixel converts any size image into a string of approximately 1kb, and uses only a MongoDB for storage - no AWS buckets necessary.</h2>
-        <h2>Built with JavaScript, HTML5, CSS3, Vue, Nuxt, MongoDB, Express, and bCrypt.</h2>
-        <h3>Please contact the creator, <a href="https://www.linkedin.com/in/sandy-lord-engineer/">Sandy Lord</a>, for any details or questions.</h3>
-        <h3>Have fun!!</h3>
+        <h1>Welcome to rgbPixel</h1><br>
+        <h2>rgbPixel allows you to save and animate your own 16x16 pictures, either hand created or converted from an image.</h2><br>
+        <h2>rgbPixel converts any size image into a string of approximately 1kb, and uses only a MongoDB for storage - no AWS buckets necessary.</h2><br>
+        <h2>Built with JavaScript, HTML5, CSS3, Vue, Nuxt, MongoDB, Express, and bCrypt.</h2><br>
+        <h3>Please contact the creator, <a href="https://www.linkedin.com/in/sandy-lord-engineer/">Sandy Lord</a>, for any details or questions.</h3><br>
+        <h3>Have fun!!</h3><br>
     </main>
 </template>
 
@@ -20,6 +20,8 @@ export default {
 </script>
 
 <style scoped>
-
+h2, h3 {
+    font-family: Arial, Helvetica, sans-serif;
+}
 
 </style>
